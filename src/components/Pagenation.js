07cb5ebe78f452f6b387e23502fc587420bbcbb2
iiishokoiii <tobby_36@yahoo.co.jp>
2022-08-07 11:44:13 +0900

    ページネーション（prev/next追加、スタイリング）

--- src/components/Pagenation.js
+++ src/components/Pagenation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from '../styles/pagenation.module.scss';
 
 function Pagenation(props) {
   const { itemCount, itemCountPerPage, currentIndex, onClick } = props;
@@ -7,18 +8,42 @@ function Pagenation(props) {
     return [...Array(rangeMax)].map((_, idx) => idx + 1);
   };
   return (
-    <ul>
-      {pageIndexArr(rangeMax).map((item) => (
-        <li
-          className={item === currentIndex ? 'text-blue-500' : 'hoge'}
-          key={item.id}
-        >
-          <button type="button" onClick={onClick(item)}>
-            {item}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className={styles.list}>
+        {currentIndex > 1 ? (
+          <li  className={styles.item}>
+            <button type="button" onClick={onClick(currentIndex - 1)}>
+              prev
+            </button>{' '}
+          </li>
+        ) : (
+          ''
+        )}
+
+        {pageIndexArr(rangeMax).map((item) => (
+          <li
+            className={`${styles.item} ${
+              item === currentIndex ? styles.current : ''
+            }`}
+            key={item.id}
+          >
+            <button type="button" onClick={onClick(item)}>
+              {item}
+            </button>
+          </li>
+        ))}
+
+        {currentIndex <rangeMax ? (
+         <li  className={styles.item}>
+            <button type="button" onClick={onClick(currentIndex + 1)}>
+              next
+            </button>{' '}
+          </li>
+        ) : (
+          ''
+        )}
+      </ul>
+    </>
   );
 }
 export default Pagenation;
